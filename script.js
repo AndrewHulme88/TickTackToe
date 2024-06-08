a10c9c11ecbbb2d16234542170c62599bafa8333
andrew hulme <andrew_hulme04@hotmail.com>
2024-06-08 11:10:53 +1000

    Setup gameboard

---
+++ script.js
@@ -0,0 +1,11 @@
+const Gameboard = (() => {
+  let board = ["", "", "", "", "", "", "", "", ""];
+
+  const getBoard = () => board;
+
+  const resetBoard = () => {
+    board = ["", "", "", "", "", "", "", "", ""];
+  };
+
+
+})
